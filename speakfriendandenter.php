2193caea9547bb2c33594120e2d4063b5f85b9d7
RogerAntonio <roger.antonio@live.be>
2015-08-24 12:18:47 +0200

    Login page

---
+++ speakfriendandenter.php
@@ -0,0 +1,20 @@
+<?php
+
+session_start();
+
+use Doctrine\Common\ClassLoader;
+
+/* Initializing Doctrine */
+require_once'Doctrine/Common/ClassLoader.php';
+$classLoader = new ClassLoader("source");
+$classLoader->register();
+
+/* Initializing Twig */
+require_once("lib/Twig/Autoloader.php");
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem("source/presentation");
+$twig = new Twig_Environment($loader);
+$view = $twig->render("login.twig", array());
+
+print($view);
